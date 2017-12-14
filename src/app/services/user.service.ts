9bf20923851fd9aaaebbed5aa657e2eac73e2233
kongkong556 <kongdumri.kdr@g.swu.ac.th>
2017-12-14 15:34:31 +0700

    showdb

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -18,6 +18,10 @@ export class UserService {
     return this.http.post<User>("http://localhost:9990/users" , user)
   }
 
+  getUsers() : Observable<User[]>{
+    return this.http.get<User[]>("http://localhost:9990/users")
+  }
+
   
 
 }
