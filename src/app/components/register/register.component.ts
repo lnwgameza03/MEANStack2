6be0bce21b09ed8f9179c07c591e4c9a5d35000a
nattapong lahoongpase <nattapong.lahoongpase@g.swu.ac.thgit config --global user.email nattapong.laho>
2017-12-16 01:57:41 +0700

    register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
-
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,13 +12,47 @@ import { User } from '../../models/user.model';
 export class RegisterComponent implements OnInit {
 
   user = new User()
-
-  constructor(private userService:UserService) { }
+  err : string[] = [];
+  constructor(private userService:UserService,private router:Router) { }
 
   ngOnInit() {
+    this.user = new User();
   }
 
   saveRegist(){
+    this.err = [];
+    if(!this.user.email){
+      this.err.push("กรุณากรอกอีเมล")
+      return;
+    }
+    if(!this.user.password){
+      this.err.push("กรุณากรอกรหัสผ่าน")
+      return;
+    }
+    if(!this.user.userName){
+      this.err.push("กรุณากรอกชื่อผู้ใช้")
+      return;
+    }
+    if(!this.user.firstName){
+      this.err.push("กรุณากรอกชื่อ")
+      return;
+    }
+    if(!this.user.lastName){
+      this.err.push("กรุณากรอกนามสกุล")
+      return;
+    }
+    if(!this.user.DOB){
+      this.err.push("กรุณากรอกวันเดือนปีเกิด")
+      return;
+    }
+    if(!this.user.phone){
+      this.err.push("กรุณากรอกเบอร์โทรศัพท์")
+      return;
+    }
+    
+    this.userService.Create(this.user).subscribe(user =>  {this.router.navigate(['/home'])}, err => {
+      this.err.push(`This email already exists`);
+    })
 
     
 
@@ -29,7 +63,7 @@ export class RegisterComponent implements OnInit {
     // this.user.DOB = ((<HTMLTextAreaElement>document.getElementById('date')).value)
     // this.user.phone = ((<HTMLTextAreaElement>document.getElementById('phone')).value)
     //this.user.email = ((<HTMLTextAreaElement>document.getElementById('email')).value)
-    this.userService.Create(this.user).subscribe(user => (null))
+    
   }
 
 }
